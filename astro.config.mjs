7461f3884798cad67a2f45df696605c19c059054
Max <naturale@hufs.ac.kr>
2025-07-11 17:27:44 +0900

    fixed path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maximizemaxwell.github.io',
-  base: '/cv',
   vite: {
     plugins: [tailwindcss()]
   }
